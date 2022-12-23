56439c4135be26f1827d7d9ae6754f3362af14e4
Anirudh <anirudhmukkamala@gamil.com>
2022-12-23 11:37:13 +0530

    Something is more done

--- env_in/Eco_Info/urls.py
+++ env_in/Eco_Info/urls.py
@@ -4,4 +4,6 @@ from .views import *
 app_name = "Eco_Info"
 urlpatterns = [
     path('',Home,name="Home"),
+    path('blogs/',Blogs,name="Blogs"),
+    path('learn/',Learn,name="Learn"),
 ]
--- env_in/Eco_Info/views.py
+++ env_in/Eco_Info/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request,"Eco_Info/home.html")
+
+def Blogs(request):
+    return render(request,"Eco_Info/Blogs/index.html")
+
+def Learn(request):
+    return render(request,"Eco_Info/Learn/index.html")
